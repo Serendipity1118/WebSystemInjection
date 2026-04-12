69dc0f6d1402541f2e97a42fa0a07a2e8fbd7cda
Serendipity1118 <develop@serendipy.jp>
2026-04-12 11:41:16 +0900

    Delete setup-git-takayanagi.ps1

--- setup-git-takayanagi.ps1
+++
@@ -1,5 +0,0 @@
-﻿chcp 65001 > $null
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-git config user.name "takayanagi"
-git config user.email "k.takayanagi@serendipy.jp"
-Write-Host "個人アカウント設定完了: $(git config user.email)" -ForegroundColor Green
