02bdb8a0e4b100d1702499c05a4570e724b80f95
Serendipity1118 <develop@serendipy.jp>
2026-04-12 11:41:00 +0900

    Delete setup-git-seren.ps1

--- setup-git-seren.ps1
+++
@@ -1,5 +0,0 @@
-﻿chcp 65001 > $null
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-git config user.name "seren"
-git config user.email "develop@serendipy.jp"
-Write-Host "会社アカウント設定完了: $(git config user.email)" -ForegroundColor Green
